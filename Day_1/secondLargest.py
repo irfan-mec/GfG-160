08bbaa6aea16def9f14cf2b73e90282fd813892c
Irfan habeeb <irfanhabeeb.mec@gmail.com>
2024-11-27 01:42:59 +0530

    Update secondLargest.py

--- Day_1/secondLargest.py
+++ Day_1/secondLargest.py
@@ -1,1 +1,31 @@
+#User function Template for python3
+class Solution:
+    def getSecondLargest(self, arr):
+        n=len(arr)
+        
+        largest=-1
+        secondLargest=-1
+        
+        for i in range(n):
+            if arr[i] > largest:
+                secondLargest=largest
+                largest=arr[i]
+            elif arr[i] < largest and arr[i] > secondLargest:
+                secondLargest = arr[i] 
+        ans=secondLargest
+        return ans
 
+
+#{ 
+ # Driver Code Starts
+# Initial Template for Python 3
+
+if __name__ == "__main__":
+    t = int(input())
+    for _ in range(t):
+        arr = list(map(int, input().split()))
+        ob = Solution()
+        ans = ob.getSecondLargest(arr)
+        print(ans)
+        print("~")
+# } Driver Code Ends
